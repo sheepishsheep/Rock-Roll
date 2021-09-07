eac6677bf4295dacb75cf3eafa475d6bcebcd893
SheepishSheep <79867219+sheepishsheep@users.noreply.github.com>
2021-09-07 17:11:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#D&D Discord Bot - Music playing and gifs
+#Author: Z. Wool
+#End goals: 
+
+
+import discord #discord lib
+import giphy_client #gif lib
+import os #token lib
+import random #random lib
+import requests #request lib
+import json #json lib
+import youtube-dl #
+
